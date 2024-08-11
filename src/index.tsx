4056a209bd6ad8757a401f9a30941898e344abb5
Jeffvo <jeffrey.oosterwijk@hotmail.com>
2024-08-11 20:54:43 +0100

    Updates

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
-import './index.css';
-
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<App />);
-}
